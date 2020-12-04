895f9daceae1099f797bf50bc36b096f7c82d180
Scott Griffin <mscottgriffin@gmail.com>
2020-12-04 00:14:41 -0500

    :zap: added pageload function to preserve reveal button between pages

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function clickreveal() {
-  let revealBtn = document.querySelector('.reveal-button');
-  let codeText = document.querySelector('.code-text');
+const revealBtn = document.querySelector('.reveal-button');
+const codeText = document.querySelector('.code-text');
+
+const pageLoad = () => {
+  if (revealBtn.classList.contains('hide')) {
+    revealBtn.classList.replace('hide', 'show');
+  }
+  if (codeText.classList.contains('show')) {
+    codeText.classList.replace('show', 'hide');
+  }
+};
+
+const clickreveal = () => {
   revealBtn.classList.replace('show', 'hide');
   codeText.classList.replace('hide', 'show');
-}
+};
