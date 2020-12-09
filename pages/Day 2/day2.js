910f605a338a46c7e7ce94154c73d2dc915c4ba4
Scott Griffin <mscottgriffin@gmail.com>
2020-12-09 00:31:10 -0500

    day 2 puzzle part 1 done

--- pages/Day 2/day2.js
+++ pages/Day 2/day2.js
@@ -1001,13 +1001,11 @@ let list = `5-11 t: glhbttzvzttkdx
 ///these steps could probably be combined, but this should be easy to follow
 // 1. remove the dashes and colons, make list into an array seperated by line break
 let data = list.replace(/\-/g, ' ').replace(/\:/g, '').split`\n`;
-
 // 2. split each line into its own array
 let array = [];
 data.forEach(function (i) {
   array.push(i.split` `);
 });
-
 //3. Create an object for each key:value pair, then combine all four pairs to one object. These objects are now indexes in a new array 'passwords'
 let passwords = [];
 array.forEach(function (i) {
@@ -1018,17 +1016,27 @@ array.forEach(function (i) {
   let entry = Object.assign({}, minObj, maxObj, charObj, passObj);
   passwords.push(entry);
 });
-console.log(passwords);
 
 ///Counters for # of valid passwords
 let validCount = 0;
-let invalidCount = 0;
-let notnull = [];
+//iterate through array, checking how many times the 'char' property of each entry object appears in the 'pass' property
 passwords.forEach((item) => {
   const matches = item.pass.match(new RegExp(item.char, 'g'));
   if (matches !== null) {
-    let matchlength = matches.length;
+    //adds a new property to note number of character matches. null is 0
+    item.matches = matches.length;
   } else {
-    invalidCount++;
+    item.matches == 0;
+  }
+  //checks if the number of matches is within the object's min-max rule, adds a new property to indicate validity
+  if (item.matches <= item.max && item.matches >= item.min) {
+    item.validity = 'valid';
+    validCount++;
+  } else {
+    item.validity = 'invalid';
   }
 });
+//an array with objects for each entry, noting validity. This is a better data set than the initial 'list' input
+console.log(passwords);
+//here's the solution!
+console.log(validCount);
