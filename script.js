3e9335abc194fb854f2aef71a71e01598633aa47
Scott Griffin <mscottgriffin@gmail.com>
2020-12-11 15:20:40 -0500

    tabs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const revealBtn = document.querySelector('.reveal-button');
-const codeText = document.querySelector('.code-text');
+const revealBtn = document.getElementsByClassName('.reveal-button');
+const codeText = document.getElementsByClassName('.code-text');
+const defaultOpen = document.getElementsByClassName('.defaultOpen');
 
 const pageLoad = () => {
   if (revealBtn.classList.contains('hide')) {
@@ -10,7 +11,31 @@ const pageLoad = () => {
   }
 };
 
+function defaultTab(){
+  defaultOpen.click()
+}
+
 const clickreveal = () => {
   revealBtn.classList.replace('show', 'hide');
   codeText.classList.replace('hide', 'show');
 };
+
+function openTab(evt, tabNum) {
+  let tabcontent, tablinks, i
+
+  // Get all elements with class="tabcontent" and hide the
+  for ( let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.querySelector('#tabNum').style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
+
